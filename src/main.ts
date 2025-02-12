af7c1b0de03ff2f2c9af39d8c2424e59c642b2cf
Muhammad Habeeb <allyearmustobey@gmail.com>
2025-02-12 11:12:11 +0100

    complete project scaffolding with nx

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import { Hono } from 'hono';
+import { serve } from '@hono/node-server';
+const app = new Hono();
+const PORT = Number(process.env.port) || 3030;
+app.get('/', (c) => {
+  return c.text('Hello world');
+});
+
+app.get('/me', (c) => {
+  return c.json({ name: 'habeeb' });
+});
+
+serve({ fetch: app.fetch, port: PORT });
+export default app;
