7ffbaffc25b447b0f3042605fb9cab025eac1960
Muhammad Habeeb <allyearmustobey@gmail.com>
2025-04-01 18:04:49 +0100

    complete workspace crud API testing

--- packages/database/schema/acceptOrRejReq.schema.ts
+++ packages/database/schema/acceptOrRejReq.schema.ts
@@ -1,8 +1,8 @@
 import z from 'zod';
 const acceptOrRejectReq = z.object({
-  signal: z.string(),
-  channelId: z.string(),
-  userId: z.string(),
+  signal: z.string({ message: 'property signal is required' }),
+  channelId: z.string({ message: 'property channelId is required' }),
+  userId: z.string({ message: 'property userId is required' }),
 });
 
 const acceptOrRejectReqValidator = (
